090769bf8ca80b12250154dd91282794d47ef107
JD <jayprowings@gmail.com>
2021-10-08 20:26:45 +0530

    new commit

--- src/com/pro/wings/MyFirstProgram.java
+++ src/com/pro/wings/MyFirstProgram.java
@@ -5,5 +5,7 @@ public class MyFirstProgram {
     public static void main(String[] args) {
         System.out.println("Hey Java developer... Welcome to github!!!");
         System.out.println("Code pushed to remote!!!");
+
+        System.out.println("New line addded");
     }
 }
